83f5a433c3163f0cfb77d3b26bbdf0d38002dfed
Ousmanly <lyousmane492@gmail.com>
2024-09-27 00:40:18 +0200

    Added cors

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ import bodyParser from 'body-parser';
 import RecipeRouter from './routes/RecipeRoute.js';
 import Categoryrouter from './routes/CategoryRoute.js';
 // import CategoryRouter from './routes/CategoryRoute.js'
+import cors from 'cors';
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
+
+app.use(cors(corsOptions));
 app.use('/api', RecipeRouter);
 app.use('/api', Categoryrouter);
 console.log('samarrchhheeee');
