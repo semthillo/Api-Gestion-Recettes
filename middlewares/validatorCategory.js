906d7188e291e6f683ea786d89ee0b8ee3b20225
Ousmanly <lyousmane492@gmail.com>
2024-09-26 05:16:37 +0200

    Added validatorCategory

---
+++ middlewares/validatorCategory.js
@@ -0,0 +1,109 @@
+import { check, param, validationResult } from 'express-validator';
+import { StatusCodes } from 'http-status-codes';
+import CategoryModel from '../models/CategoryModel.js';
+// import RecipeController from "../controllers/RecipeController.js";
+// import RecipeModel from '../models/RecipeModel.js';
+
+
+const addRequestCategoryValidatore = [
+  check('name')
+    .not()
+    .isEmpty()
+    .withMessage('name is required!')
+    .bail()
+    .isString()
+    .withMessage("name can't be number!")
+    .bail()
+    .isLength({ min: 3 })
+    .withMessage('name must be at least 3 characters long!')
+    .bail()
+    .custom(async (value) => {
+      const result = await CategoryModel.checkCategory(value);
+      if (result !== 0) {
+        throw new Error('This category is already exist!');
+      }
+      return true;
+    }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res
+        .status(StatusCodes.UNPROCESSABLE_ENTITY)
+        .json({ errors: errors.array() });
+    next();
+  },
+];
+const updateRequestCategoryValidatore = [
+  check('id')
+    .not()
+    .isEmpty()
+    .withMessage('Id is required!')
+    .bail()
+    .isInt()
+    .withMessage('Id must be number!')
+    .bail()
+    .custom(async (value) => {
+      const result = await CategoryModel.checkCategoryById(value);
+      if (result == 0) {
+        throw new Error('Category not found!');
+      }
+      return true;
+    }),
+  check('name')
+    .not()
+    .isEmpty()
+    .withMessage('name is required!')
+    .bail()
+    .isString()
+    .withMessage("name can't be number!")
+    .bail()
+    .isLength({ min: 3 })
+    .withMessage('name must be at least 3 characters long!')
+    .bail()
+    .custom(async (value) => {
+      const result = await CategoryModel.checkCategory(value);
+      if (result !== 0) {
+        throw new Error('This category is already exist!');
+      }
+      return true;
+    }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res
+        .status(StatusCodes.UNPROCESSABLE_ENTITY)
+        .json({ errors: errors.array() });
+    next();
+  },
+];
+const deleteRequestCategoryValidatore = [
+  check('id')
+    .not()
+    .isEmpty()
+    .withMessage('Id is required!')
+    .bail()
+    .isInt()
+    .withMessage('Id must be number!')
+    .bail()
+    .custom(async (value) => {
+      const result = await CategoryModel.checkCategoryById(value);
+      if (result == 0) {
+        throw new Error('Category not found!');
+      }
+      return true;
+    }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res
+        .status(StatusCodes.UNPROCESSABLE_ENTITY)
+        .json({ errors: errors.array() });
+    next();
+  },
+];
+
+export {
+  addRequestCategoryValidatore,
+  deleteRequestCategoryValidatore,
+  updateRequestCategoryValidatore,
+};
--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -7,19 +7,20 @@ import express from 'express';
 // } from '../middlewares/validatorRecipe.js';
 // import RecipeController from '../controllers/recipeController.js';
 import CategoryController from '../controllers/CategoryController.js';
+import { addRequestCategoryValidatore, deleteRequestCategoryValidatore, updateRequestCategoryValidatore } from '../middlewares/validatorCategory.js';
 
 // const app = express()
 const Categoryrouter = express.Router();
 
 Categoryrouter.get('/categories', CategoryController.getAllCategories);
-Categoryrouter.get('/categories/:id', CategoryController.getCategoryById);
-Categoryrouter.post('/categories', CategoryController.createCategories);
+Categoryrouter.get('/categories/:id',deleteRequestCategoryValidatore, CategoryController.getCategoryById);
+Categoryrouter.post('/categories', addRequestCategoryValidatore, CategoryController.createCategories);
 Categoryrouter.delete(
-  '/categories/:id',
+  '/categories/:id', deleteRequestCategoryValidatore,
   CategoryController.deleteCategories
 );
 Categoryrouter.put(
-  '/categories/:id',
+  '/categories/:id', updateRequestCategoryValidatore,
   CategoryController.updateCategories
 );
 
